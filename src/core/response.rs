3e094584c7fc4fe5f778f8d977538ae71d53cfec
Utpal Sarkar <utpalsarkar010@gmail.com>
2025-10-02 13:01:24 +0600

    converted into enum style

--- src/core/response.rs
+++ src/core/response.rs
@@ -1,32 +1,35 @@
 use std::{borrow::Cow, fmt::Display};
 
-#[derive(Debug, Clone)]
-pub struct Response {
-    pub status: usize,
-    pub message: Cow<'static, str>,
-    pub rfc: Option<Cow<'static, str>>,
-    raw: bool,
+#[derive(Debug, Clone, Default)]
+pub enum Response {
+    #[default]
+    Default,
+    Custom {
+        status: usize,
+        message: Cow<'static, str>,
+        rfc: Option<Cow<'static, str>>,
+    },
+    Ok(Cow<'static, str>),
+    Info(Cow<'static, str>),
+    Raw(Cow<'static, str>),
 }
 
 impl Display for Response {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.raw {
-            return write!(f, "{}", self.message);
+        match self {
+            Response::Raw(s) => return write!(f, "{s}"),
+            Response::Custom { status, message, rfc } => {
+                let rfc_suffix = rfc
+                    .as_ref()
+                    .filter(|s| !s.is_empty())
+                    .map(|s| format!(" {s}"))
+                    .unwrap_or_default();
+                write!(f, "{status}{rfc_suffix} {message}")
+            }
+            Response::Info(msg) => write!(f, "220 {msg}"),
+            Response::Ok(msg) => write!(f, "250 2.0.0 {msg}"),
+            Response::Default => Ok(()),
         }
-
-        let rfc = self
-            .rfc
-            .as_ref()
-            .and_then(|s| {
-                if !s.is_empty() {
-                    Some(format!(" {}", s))
-                } else {
-                    None
-                }
-            })
-            .unwrap_or_default();
-
-        write!(f, "{}{} {}", self.status, rfc, self.message)
     }
 }
 
@@ -36,20 +39,19 @@ impl Response {
         message: impl Into<Cow<'static, str>>,
         rfc: impl Into<Option<Cow<'static, str>>>,
     ) -> Self {
-        Self {
+        Self::Custom {
             status,
             message: message.into(),
             rfc: rfc.into(),
-            raw: false,
         }
     }
 
     pub fn ok(message: impl Into<Cow<'static, str>>) -> Self {
-        Self::new(250, message, Some("2.0.0".into()))
+        Self::Ok(message.into())
     }
 
     pub fn info(message: impl Into<Cow<'static, str>>) -> Self {
-        Self::new(220, message, None)
+        Self::Info(message.into())
     }
 
     pub fn reject(message: impl Into<Cow<'static, str>>) -> Self {
@@ -70,14 +72,5 @@ impl Response {
 
     pub fn bad_sequence(message: impl Into<Cow<'static, str>>) -> Self {
         Self::new(503, message, Some("5.5.1".into()))
-    }
-
-    pub(crate) fn raw(res: String) -> Self {
-        Self {
-            status: 0,
-            message: res.into(),
-            rfc: None,
-            raw: true,
-        }
     }
 }
